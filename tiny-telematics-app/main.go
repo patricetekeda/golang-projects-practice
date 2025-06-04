d42daf11425448d0b6ae82fe358c2446f46dd21b
Patrice Tekeda <104334165+patricetekeda@users.noreply.github.com>
2025-06-04 10:01:45 -0400

    Refactor main function to load configuration and run handlers

--- tiny-telematics-app/main.go
+++ tiny-telematics-app/main.go
@@ -1,20 +1,37 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"log"
+// 	"net/http"
+
+// 	"github.com/gorilla/mux"
+// 	"github.com/patriceteda/tiny-telematics-app/handlers"
+// )
+
+// func main() {
+// 	r := mux.NewRouter()
+
+// 	r.HandleFunc("/register", handlers.RegisterCar).Methods("POST")
+// 	r.HandleFunc("/cars", handlers.ListCars).Methods("GET")
+
+// 	fmt.Println("Server running at http://localhost:8080")
+// 	log.Fatal(http.ListenAndServe(":8080", r))
+// }
+
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-
-	"github.com/gorilla/mux"
-	"github.com/patriceteda/tiny-telematics-app/handlers"
+"log"
+"github.com/patriceteda/tiny-telematics-app/handlers"
 )
 
 func main() {
-	r := mux.NewRouter()
-
-	r.HandleFunc("/register", handlers.RegisterCar).Methods("POST")
-	r.HandleFunc("/cars", handlers.ListCars).Methods("GET")
-
-	fmt.Println("Server running at http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	cfg, err := config.LoadConfig()
+	if err != nil {
+         log.Fatalf("Error loading config: %v", err)
+	}
+	handlers.Run(cfg)
 }
+
+
