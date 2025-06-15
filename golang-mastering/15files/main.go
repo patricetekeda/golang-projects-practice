b89643517b76c0d2616447de3577d306f34c5945
Patrice Tekeda <104334165+patricetekeda@users.noreply.github.com>
2025-06-15 12:01:56 -0400

    Add web content retrieval and URL parsing functionality in Go

--- golang-mastering/15files/main.go
+++ golang-mastering/15files/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"io"
+	"net/http"
+	"net/url"
 	"os"
 )
 
@@ -13,32 +15,78 @@ func main() {
 	content := "This is a sample content for the file."
 
 	file, err := os.Create("./sample.txt")
-	if err != nil {  
-		panic(err)    
-	}
+	CheckError(err)
 	length, err := io.WriteString(file, content)
-	checkError(err)
+	CheckError(err)
 	fmt.Println("File created successfully")
 	fmt.Printf("File created with length: %d\n", length)
     defer file.Close()
 
-	readFile("./sample.txt")
+	// readFile("./sample.txt")
+	// GetWebcontent()
+	ParseUrl()
 
 }
 
 func readFile( fileName string) {
 	content, err := os.ReadFile(fileName)
-	checkError(err)
+	CheckError(err)
 	fmt.Println("File Read Successfully")
 	fmt.Println("File Content:", string(content))
 	defer os.Remove(fileName) // Clean up the file after reading
 	
 }
 
-func checkError(err error) {
+func CheckError(err error) {
 	if err != nil {
 		panic(err)
 	}
 }
+
+// Handling web request in golang
+const postsURL string = "https://jsonplaceholder.typicode.com/posts"
+
+func GetWebcontent() {
+	resp, err := http.Get(postsURL)
+	CheckError(err)
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	CheckError(err)
+	fmt.Println("Web Content Retrieved Successfully")
+	fmt.Println("Web Content:", string(body))
+
+}
+
+// Handling url in golang
+// url with query string or parameters
+const url1 string = "https://jsonplaceholder.typicode.com/posts?userId=1"
+
+func ParseUrl() {
+	res, err := url.Parse(url1)
+	CheckError(err)
+	fmt.Println("Parsed URL:", res)
+	fmt.Println("Scheme:", res.Scheme)
+	fmt.Println("Host:", res.Host)
+	fmt.Println("Path:", res.Path)
+	fmt.Println("Query:", res.Query())
+	fmt.Println("User ID:", res.Query().Get("userId"))
+	fmt.Println("Fragment:", res.Fragment)
+	fmt.Println("Raw Query:", res.RawQuery)
+	fmt.Println("Raw Path:", res.RawPath)
+	fmt.Println("String Representation:", res.String())
+	fmt.Println("Host with Port:", res.Hostname(), ":", res.Port())
+	fmt.Println("URL String:", res.String())
+	fmt.Println("URL String with Query:", res.String()+"?"+res.RawQuery)
+	fmt.Println("URL String with Fragment:", res.String()+"#"+res.Fragment)
+	fmt.Println("URL String with Path:", res.String()+res.Path)
+}
+
+
+
+
+
+
+
 
 
